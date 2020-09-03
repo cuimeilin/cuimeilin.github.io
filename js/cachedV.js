b7001306e9b5bf6bed226520a8f751181bd486b9
cuimeilin <36574410+cuimeilin@users.noreply.github.com>
2020-09-02 20:54:38 -0700

    deleted old files

--- js/cachedV.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('pageshow', function (event) {
-  if (!event.persisted) {
-    return;
-  }
-  var fader = document.getElementById('fader');
-  fader.classList.remove('fade-in');
-});
