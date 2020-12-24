08b2664148d7d2d4941c3f408f4dc7d6eba0bde7
Meilin <cuimeilin17@gmail.com>
2020-12-23 20:58:57 -0800

    Updates

--- precache-manifest.51b7b776b21e925b714a1c95bf015813.js
+++ precache-manifest.51b7b776b21e925b714a1c95bf015813.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f4ae7e7d60d9221f070212eac2e83dfd",
+    "revision": "a976c2cdf7e709cd451fd463d741a1b3",
     "url": "/index.html"
   },
   {
-    "revision": "6b0fe8a02e1368cf65fd",
-    "url": "/static/css/main.b3881743.chunk.css"
+    "revision": "97bd99f04d5a9bb63c1d",
+    "url": "/static/css/main.02294628.chunk.css"
   },
   {
     "revision": "7f2c122768bac50c1ac0",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.e3b844bf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6b0fe8a02e1368cf65fd",
-    "url": "/static/js/main.9046c59c.chunk.js"
+    "revision": "97bd99f04d5a9bb63c1d",
+    "url": "/static/js/main.36255bcf.chunk.js"
   },
   {
     "revision": "5b1dc7159f9666505c77",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cabd65afc6b10645c3efe474473ee43.js"
+  "/precache-manifest.51b7b776b21e925b714a1c95bf015813.js"
 );
 
 self.addEventListener('message', (event) => {
